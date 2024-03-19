7a6ee0c38bf4159e0868c8711dae93a486f53b5d
thomasjm <tom@codedown.io>
2024-03-19 00:30:04 -0700

    Static build progress

--- flake.nix
+++ flake.nix
@@ -1,24 +1,40 @@
 {
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/release-23.05";
-
   inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.gitignore = {
+    url = "github:hercules-ci/gitignore.nix";
+    inputs.nixpkgs.follows = "nixpkgs";
+  };
+  inputs.haskellNix.url = "github:input-output-hk/haskell.nix";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/release-23.11";
 
-  outputs = { self, nixpkgs, flake-utils }@inputs:
+  outputs = { self, flake-utils, gitignore, haskellNix, nixpkgs }@inputs:
     # flake-utils.lib.eachDefaultSystem (system:
     flake-utils.lib.eachSystem ["x86_64-linux"] (system:
       let
-        pkgs = import nixpkgs { inherit system; };
-        default = pkgs.haskell.packages.ghc8107.callPackage ./default.nix {};
+        overlays = [
+          haskellNix.overlay
+          (import ./nix/fix-ghc-pkgs-overlay.nix)
+        ];
+
+        pkgs = import nixpkgs { inherit system overlays; inherit (haskellNix) config; };
+
+        src = gitignore.lib.gitignoreSource ./.;
+
+        flake = (pkgs.hixProject "ghc964" src).flake {};
+        flakeStatic = (pkgs.pkgsCross.musl64.hixProject "ghc964" src).flake {};
+
       in
         rec {
           packages = rec {
+            inherit pkgs flake flakeStatic;
+
             inherit (pkgs) cabal2nix;
-            inherit default;
+
+            normal = flake.packages."sauron:exe:sauron";
+            static = flakeStatic.packages."sauron:exe:sauron";
 
             print-nixpkgs = pkgs.writeShellScriptBin "print-nixpkgs.sh" "echo ${pkgs.path}";
           };
-
-          defaultPackage = packages.default;
         }
     );
 }
