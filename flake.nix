acda3210e5a029b5cf6beb07bf3b05d6f6d96341
Tom McLaughlin <tom@codedown.io>
2025-01-31 04:48:41 -0800

    Managed to cross-compile for Windows!

--- flake.nix
+++ flake.nix
@@ -5,24 +5,70 @@
     inputs.nixpkgs.follows = "nixpkgs";
   };
   inputs.haskellNix.url = "github:input-output-hk/haskell.nix";
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/release-24.05";
+  inputs.nixpkgs.follows = "haskellNix/nixpkgs-unstable";
 
   outputs = { self, flake-utils, gitignore, haskellNix, nixpkgs }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        overlays = [
-          haskellNix.overlay
-          (import ./nix/fix-ghc-pkgs-overlay.nix)
-        ];
-
-        pkgs = import nixpkgs { inherit system overlays; inherit (haskellNix) config; };
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [
+            haskellNix.overlay
+            (import ./nix/fix-ghc-pkgs-overlay.nix)
+          ];
+          inherit (haskellNix) config;
+        };
 
         src = gitignore.lib.gitignoreSource ./.;
 
         compilerNixName = "ghc966";
 
-        flake = (pkgs.hixProject compilerNixName src).flake {};
-        flakeStatic = (pkgs.pkgsCross.musl64.hixProject compilerNixName src).flake {};
+        flake = (pkgs.haskell-nix.hix.project {
+          inherit src;
+          evalSystem = "x86_64-linux";
+          compiler-nix-name = compilerNixName;
+          projectFileName = "stack.yaml";
+          modules = [
+            (import ./nix/fix-ghc-pkgs-module.nix)
+            {
+              packages.sauron.components.exes.sauron.configureFlags = [
+                ''--ghc-options="-pgml g++"''
+              ];
+              packages.sauron.components.exes.sauron.build-tools = [pkgs.gcc];
+              packages.sauron.components.exes.sauron.dontStrip = false;
+            }
+          ];
+        }).flake {};
+
+        flakeStatic = (pkgs.pkgsCross.musl64.haskell-nix.hix.project {
+          inherit src;
+          evalSystem = "x86_64-linux";
+          compiler-nix-name = compilerNixName;
+          projectFileName = "stack.yaml";
+          modules = [
+            (import ./nix/fix-ghc-pkgs-module.nix)
+            {
+              packages.sauron.components.exes.sauron.configureFlags = [
+                ''--ghc-options="-pgml g++"''
+              ];
+              packages.sauron.components.exes.sauron.build-tools = [pkgs.gcc];
+              packages.sauron.components.exes.sauron.dontStrip = false;
+            }
+          ];
+        }).flake {};
+
+        flakeWindows = (pkgs.pkgsCross.mingwW64.haskell-nix.hix.project {
+          inherit src;
+          compiler-nix-name = compilerNixName;
+          projectFileName = "stack.yaml";
+          modules = [
+            {
+              packages.bitvec.components.library.configureFlags = [
+                "-f -simd"
+              ];
+            }
+          ];
+        }).flake {};
 
       in
         {
@@ -33,6 +79,7 @@
 
             normal = flake.packages."sauron:exe:sauron";
             static = flakeStatic.packages."sauron:exe:sauron";
+            windows = flakeWindows.packages."sauron:exe:sauron";
 
             print-nixpkgs = pkgs.writeShellScriptBin "print-nixpkgs.sh" "echo ${pkgs.path}";
           };
