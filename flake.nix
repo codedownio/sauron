2d6b1fd507111be6e5d3ba1d02f05d5f4a2c0cb3
Tom McLaughlin <tom@codedown.io>
2025-09-26 19:22:58 -0700

    Working on semi-static darwin

--- flake.nix
+++ flake.nix
@@ -51,6 +51,58 @@
           ];
         }).flake {};
 
+        flakeDarwinStatic = (pkgs.haskell-nix.hix.project {
+          inherit src;
+          evalSystem = "x86_64-linux";
+          compiler-nix-name = compilerNixName;
+          projectFileName = "stack.yaml";
+          modules = [
+            (import ./nix/fix-ghc-pkgs-module.nix)
+            (import ./nix/os-string-module.nix)
+            {
+              packages.sauron.components.exes.sauron.configureFlags = [
+                ''--ghc-options="-pgml g++ -optl-L${pkgs.pkgsStatic.gmp}/lib -optl-L${pkgs.pkgsStatic.ncurses}/lib -optl-L${pkgs.pkgsStatic.pcre}/lib -optl-L${pkgs.pkgsStatic.libffi}/lib -optl-L${pkgs.pkgsStatic.gettext}/lib"''
+                ''--ghc-options="-optl-lgmp -optl-lncursesw -optl-lpcre -optl-lffi -optl-lintl"''
+                "--disable-shared"
+              ];
+              packages.sauron.components.exes.sauron.build-tools = [pkgs.gcc];
+              packages.sauron.components.exes.sauron.dontStrip = false;
+
+              # Override C library dependencies to use static versions
+              packages.sauron.components.exes.sauron.libs = with pkgs; [
+                pkgsStatic.gmp
+                pkgsStatic.ncurses
+                pkgsStatic.pcre
+                pkgsStatic.libffi
+                pkgsStatic.gettext
+              ];
+
+              packages.sauron.components.exes.sauron.postInstall = ''
+                # Strip the binary
+                strip "$out/bin/sauron"
+
+                # Verify linking - should show system libraries only, no nix store dylibs
+                echo "=== Checking final binary dependencies ==="
+                otool -L "$out/bin/sauron"
+
+                # Count nix store references - should be minimal (ideally just system libs)
+                nix_refs=$(otool -L "$out/bin/sauron" | grep -c "/nix/store/" || echo "0")
+                echo "Nix store references found: $nix_refs"
+
+                # This is acceptable as long as there are no dylib files being shipped
+                # The goal is to not need separate dylib files
+                if [ -d "$out/bin" ] && find "$out/bin" -name "*.dylib" | grep -q .; then
+                  echo "ERROR: Dylib files found in output!"
+                  find "$out/bin" -name "*.dylib"
+                  exit 1
+                fi
+
+                echo "SUCCESS: No separate dylib files needed"
+              '';
+            }
+          ];
+        }).flake {};
+
         flakeStatic = (pkgs.pkgsCross.musl64.haskell-nix.hix.project {
           inherit src;
           evalSystem = "x86_64-linux";
@@ -110,12 +162,13 @@
           };
 
           packages = rec {
-            inherit pkgs flake flakeStatic;
+            inherit pkgs flake flakeStatic flakeDarwinStatic;
 
             inherit (pkgs) cabal2nix;
 
             normal = flake.packages."sauron:exe:sauron";
             static = flakeStatic.packages."sauron:exe:sauron";
+            darwin-static = flakeDarwinStatic.packages."sauron:exe:sauron";
             windows = flakeWindows.packages."sauron:exe:sauron";
 
             print-nixpkgs = pkgs.writeShellScriptBin "print-nixpkgs.sh" "echo ${pkgs.path}";
