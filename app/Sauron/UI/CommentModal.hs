e7aec9dcbd3f3628083505d0d04ee5b75d53721c
Tom McLaughlin <tom@codedown.io>
2025-10-24 16:08:40 -0700

    Comment modal styling

--- app/Sauron/UI/CommentModal.hs
+++ app/Sauron/UI/CommentModal.hs
@@ -18,9 +18,9 @@ renderModal appState (CommentModalState editor issueNumber isPR) = renderComment
 renderCommentModal :: AppState -> Editor Text ClickableName -> Int -> Bool -> Widget ClickableName
 renderCommentModal _appState editor issueNumber isPR =
   vBox [
-    sideBySideEditor editor issueNumber isPR,
-    hBorder,
-    buttonSection editor issueNumber isPR
+    padBottom Max $ sideBySideEditor editor issueNumber isPR
+    , hBorder
+    , buttonSection editor issueNumber isPR
   ]
   & border
   & withAttr normalAttr
@@ -32,25 +32,26 @@ sideBySideEditor :: Editor Text ClickableName -> Int -> Bool -> Widget Clickable
 sideBySideEditor editor issueNumber isPR = hBox [
   -- Left column: Editor
   vBox [
-    hCenter $ withAttr boldText $ str modalTitle,
-    hBorder,
-    padAll 1 $
-      vLimit 20 $
-      withAttr normalAttr $
-      renderEditor (str . toString . T.unlines) True editor
+    hCenter $ withAttr boldText $ str modalTitle
+    , hBorder
+    , padAll 1 $
+        vLimit 20 $
+        withAttr normalAttr $
+        renderEditor (str . toString . T.unlines) True editor
+    , fill ' '
     ]
   , vBorder
   -- Right column: Preview
   , vBox [
-    hCenter $ withAttr boldText $ str "Preview",
-    hBorder,
-    padAll 1 $
-      hLimit 60 $
-      vLimit 20 $
-      viewport CommentModal Vertical $
-      if T.null text
-        then withAttr italicText $ str "(preview will appear here)"
-        else markdownToWidgetsWithWidth 58 text
+    hCenter $ withAttr boldText $ str "Preview"
+    , hBorder
+    , padAll 1 $
+        hLimit 60 $
+        vLimit 20 $
+        viewport CommentModal Vertical $
+        if T.null text
+          then withAttr italicText $ str "(preview will appear here)"
+          else markdownToWidgetsWithWidth 58 text
     ]
   ]
   where
@@ -61,26 +62,24 @@ sideBySideEditor editor issueNumber isPR = hBox [
     text = T.unlines $ getEditContents editor
 
 buttonSection :: Editor Text ClickableName -> Int -> Bool -> Widget ClickableName
-buttonSection editor _issueNumber _isPR = hBox [
-  fill ' ',
-  hBox [
-    border $ hBox [
-      str " ",
-      withAttr hotkeyMessageAttr $ str buttonText,
-      str " [",
-      withAttr hotkeyAttr $ str "Ctrl+Shift+Enter",
-      str "] "
-    ],
-    str "  ",
-    border $ hBox [
-      str " ",
-      withAttr (if hasText then hotkeyMessageAttr else disabledHotkeyMessageAttr) $ str "Comment",
-      str " [",
-      withAttr (if hasText then hotkeyAttr else disabledHotkeyAttr) $ str "Ctrl+Enter",
-      str "] "
+buttonSection editor _issueNumber _isPR =
+  padLeft Max $ hBox [
+      border $ hBox [
+        str " "
+        , withAttr hotkeyMessageAttr $ str buttonText
+        , str " ["
+        , withAttr hotkeyAttr $ str "Ctrl+Shift+Enter"
+        , str "] "
+        ]
+      , str "  "
+      , border $ hBox [
+          str " "
+          , withAttr (if hasText then hotkeyMessageAttr else disabledHotkeyMessageAttr) $ str "Comment"
+          , str " ["
+          , withAttr (if hasText then hotkeyAttr else disabledHotkeyAttr) $ str "Ctrl+Enter"
+          , str "] "
+          ]
     ]
-  ]
-  ]
   where
     text = T.unlines $ getEditContents editor
     hasText = not $ T.null $ T.strip text
