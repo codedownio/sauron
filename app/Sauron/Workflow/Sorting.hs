7fd4a11bb92ca165312f54959b257ee949099ba0
thomasjm <tom@codedown.io>
2026-06-05 15:30:23 -0700

    Improved workflow jobs sorting

--- app/Sauron/Workflow/Sorting.hs
+++ app/Sauron/Workflow/Sorting.hs
@@ -10,27 +10,42 @@ import Data.Time.Clock (NominalDiffTime, diffUTCTime)
 import GitHub
 import Relude
 import Sauron.Types
+import Sauron.UI.Statuses (chooseWorkflowStatus)
 
 
 -- * Sorting
 
 -- | Sort key type that provides a uniform Ord instance across sort modes.
--- Sorts lexicographically: (failure grouping, runtime desc, name)
+-- Sorts lexicographically: (status grouping, runtime desc, name)
 type JobSortKey = (Int, Data.Ord.Down NominalDiffTime, Text)
+
+-- | Rank for "Sort by failures" mode, bubbling more interesting statuses to the top:
+-- failed -> running -> pending -> cancelled/neutral/unknown -> success.
+workflowStatusRank :: WorkflowStatus -> Int
+workflowStatusRank WorkflowFailed = 0
+workflowStatusRank WorkflowRunning = 1
+workflowStatusRank WorkflowPending = 2
+workflowStatusRank WorkflowCancelled = 3
+workflowStatusRank WorkflowNeutral = 4
+workflowStatusRank WorkflowUnknown = 5
+workflowStatusRank WorkflowSuccess = 6
 
 jobSortKey :: WorkflowJobSortBy -> Maybe Job -> JobSortKey
 jobSortKey workflowJobSortBy mj = case workflowJobSortBy of
   SortJobsByName -> (0, Data.Ord.Down 0, name)
-  SortJobsByRuntime -> (0, Data.Ord.Down runtime, name)
-  SortJobsByFailures -> (if isFailed then 0 else 1, Data.Ord.Down 0, name)
+  -- Jobs without a runtime yet (still running / not started) sort to the bottom.
+  SortJobsByRuntime -> case runtime of
+    Just r -> (0, Data.Ord.Down r, name)
+    Nothing -> (1, Data.Ord.Down 0, name)
+  SortJobsByFailures -> (statusRank, Data.Ord.Down 0, name)
   where
     name = maybe "" (untagName . jobName) mj
     runtime = case mj of
-      Just j -> maybe 999999 (\c -> diffUTCTime c (jobStartedAt j)) (jobCompletedAt j)
-      Nothing -> 0
-    isFailed = case mj of
-      Just j -> fromMaybe (jobStatus j) (jobConclusion j) `elem` ["failure", "timed_out"]
-      Nothing -> False
+      Just j -> (\c -> diffUTCTime c (jobStartedAt j)) <$> jobCompletedAt j
+      Nothing -> Nothing
+    statusRank = case mj of
+      Just j -> workflowStatusRank (chooseWorkflowStatus (fromMaybe (jobStatus j) (jobConclusion j)))
+      Nothing -> workflowStatusRank WorkflowUnknown
 
 sortWorkflowJobsSTM :: WorkflowJobSortBy -> [Node Variable SingleJobT] -> STM [Node Variable SingleJobT]
 sortWorkflowJobsSTM workflowJobSortBy jobs = do
